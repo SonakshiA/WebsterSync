9a39ae150ea16dafbcba01401c47fdf4911d4a36
SonakshiA <ar_sona@outlook.com>
2025-01-01 13:22:19 +0530

    Add files

---
+++ README.md
@@ -0,0 +1,35 @@
+# WebsterSync
+
+This Chrome extension allows you to add a particular word from Merriam Webster to a Supabase database!
+
+## Usage
+1. Create a Supabase account- assign any name for the organisation and project. 
+
+2. In the SQL editor in Supabase, paste the following query and run it:
+```
+CREATE TABLE words (
+  id serial PRIMARY KEY,
+  word text NOT NULL,
+  meaning text[] NOT NULL
+);
+```
+3. Clone the GitHub repository
+``` 
+git clone https://github.com/SonakshiA/webstersync.git
+``` 
+
+4. You will find the Project URL and the API Key in the Home section of Supabase, paste those in the script.js file.
+
+5. Paste the Project URL under the host_permissions section in the manifest.json file. 
+
+6. Open Chrome and select the three dots on the top right corner, from the dropdown, choose the 'Extensions' tab and select 'Manage Extensions'.
+
+7. Click on 'Load unpacked' and select the webstersync repo.
+
+8. Navigate to any Merriam-Webster word page (e.g., https://www.merriam-webster.com/dictionary/ponder).
+
+9. Click the extension icon to extract the word and its meaning.
+
+10. The word and its meaning will be stored in your Supabase database.
+
+11. You can access the stored words from the Table Editor section in Supabase. The 'words' table is present there.
---
+++ script.js
@@ -0,0 +1,82 @@
+const proxy = 'https://api.allorigins.win/raw?url=';  // Use All Origins
+const SUPABASE_URL = 'https://whmgvclpatwhwcyygdbd.supabase.co'; /* Paste the Supabase Project URL here */
+const SUPABASE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndobWd2Y2xwYXR3aHdjeXlnZGJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU3MTA5NTIsImV4cCI6MjA1MTI4Njk1Mn0.dJyoXAs7KbsIGNxwQJKtSTIhSszbPEl0BxKOrAtdLX8'; /* Paste the API Key for Supabase Database' */
+
+
+document.getElementById('loader').style.display = 'none';
+
+document.getElementById('add_word').addEventListener('click',function(){
+    document.getElementById('loader').style.display = 'block';
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        const tabUrl = tabs[0].url;  // Gets the active tab's URL
+        fetchAndExtractText(tabUrl);
+        });
+    });
+
+
+async function fetchAndExtractText(currentUrl) {
+    try{
+        const response = await fetch(proxy + currentUrl);
+        const html = await response.text();
+
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, 'text/html');
+
+        const word = doc.querySelector('.hword').textContent;
+        document.getElementById('word_title').innerHTML = word.toUpperCase();
+
+        let elements = doc.querySelectorAll('.dtText');
+        let texts = Array.from(elements).map(element => element.innerText);
+
+        //clean the extracted meanings
+        for(let i=0;i<texts.length;i++){
+            texts[i] = texts[i].replace(': ','');
+        }
+
+        let meaningsList = document.getElementById("meanings");
+        meaningsList.innerHTML = '';
+
+
+        for(let i=0;i<texts.length;i++){
+            console.log(texts[i].replace(':',''));
+            meaningsList.innerHTML += `<li>${texts[i].replace(':','')}</li>`;
+        }
+
+        if(!meaningsList){
+            document.getElementById('loader').style.display = 'block';
+        }else{
+            document.getElementById('loader').style.display = 'none';
+        }
+        saveWord(word,texts);
+
+    } catch (error){
+        console.error('Error fetching or parsing the URL:', error);
+    }
+}
+
+async function saveWord(word, texts){
+    try{
+        const response = await fetch(`${SUPABASE_URL}/rest/v1/words`,{
+            method:"POST",
+            headers: {
+                'Content-Type':'application/json',
+                'apiKey':SUPABASE_API_KEY,
+                'Authorization':`Bearer ${SUPABASE_API_KEY}`
+            },
+            body: JSON.stringify({
+                word: word,
+                meaning: texts
+            })
+        });
+        if (response.ok){
+            let databaseStatus = document.getElementById('databaseStatus');
+            databaseStatus.innerHTML = 'Word added to Supabase!';
+            console.log("Word saved!");
+        }else{
+            const errorData = await response.json();
+            console.error('Failed to save meanings:', errorData);
+        }
+    }catch(error){
+        console.log("Error thrown: ", error);
+    }
+}
